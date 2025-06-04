8eedb680842fef689faa6659fa89829864c76437
fitronansori <fitronansori7@gmail.com>
2025-06-04 14:15:37 +0800

    feat: add FeaturedNews, Categories, and About components to HomePage

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -1,10 +1,17 @@
 import Hero from "@/components/layouts/homepage/Hero";
+import FeaturedNews from "@/components/layouts/homepage/FeaturedNews";
+import Categories from "@/components/layouts/homepage/Categories";
+import About from "@/components/layouts/homepage/About";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <FeaturedNews />
+      <Categories />
+      <About />
     </>
   );
 };
+
 export default HomePage;
