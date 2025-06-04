ac2aca594cfc2d98446304415484d4766748411c
fitronansori <fitronansori7@gmail.com>
2025-06-04 11:22:16 +0800

    feat: add navigation links for main sections

---
+++ src/constants/data.ts
@@ -0,0 +1,18 @@
+export const nav_links = [
+  {
+    name: "Homey",
+    href: "/",
+  },
+  {
+    name: "Lagi Hits Nih",
+    href: "/trending",
+  },
+  {
+    name: "Paling Viral Sih",
+    href: "/populer",
+  },
+  {
+    name: "About Us Gitu Lo",
+    href: "/tentang",
+  },
+];
